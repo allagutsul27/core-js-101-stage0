5e66c3ce92b7fc2eedf4a490e025994f810b7ec0
Alla Gutsul <allagutsul1990@gmail.com>
2025-08-10 13:21:00 +0300

    add new solution

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -68,8 +68,10 @@ function getAverage(value1, value2) {
  *   (0,0) (1,0)    => 1
  *   (-5,0) (10,-10) => 18.027756377319946
  */
-function getDistanceBetweenPoints(/* x1, y1, x2, y2 */) {
-  throw new Error('Not implemented');
+function getDistanceBetweenPoints(x1, y1, x2, y2) {
+  const distance = Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2);
+
+  return distance;
 }
 
 /**
@@ -84,8 +86,8 @@ function getDistanceBetweenPoints(/* x1, y1, x2, y2 */) {
  *   x + 8 = 0       => -8
  *   5*x = 0         => 0
  */
-function getLinearEquationRoot(/* a, b */) {
-  throw new Error('Not implemented');
+function getLinearEquationRoot(a, b) {
+  return -b / a;
 }
 
 /**
@@ -106,8 +108,15 @@ function getLinearEquationRoot(/* a, b */) {
  *   (0,1) (0,1)     => 0
  *   (0,1) (1,2)     => 0
  */
-function getAngleBetweenVectors(/* x1, y1, x2, y2 */) {
-  throw new Error('Not implemented');
+function getAngleBetweenVectors(x1, y1, x2, y2) {
+  const dot = x1 * x2 + y1 * y2;
+
+  const v1 = Math.sqrt(x1 ** 2 + y1 ** 2);
+  const v2 = Math.sqrt(x2 ** 2 + y2 ** 2);
+
+  const cos = dot / (v1 * v2);
+
+  return Math.acos(cos);
 }
 
 /**
@@ -122,8 +131,12 @@ function getAngleBetweenVectors(/* x1, y1, x2, y2 */) {
  *     5     => 5
  *     0     => 0
  */
-function getLastDigit() {
-  throw new Error('Not implemented');
+function getLastDigit(num) {
+  if (String(num).length === 1) {
+    return num;
+  }
+
+  return String(num).slice(-1);
 }
 
 /**
